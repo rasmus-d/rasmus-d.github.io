4b634233025ba0a8f1ce9009d754757c1d3d64d2
rasdan <rasdan@kth.se>
2023-05-09 21:01:39 +0200

    webbutveckling är inte riktigt min grej...

--- index.js
+++
@@ -1,9 +0,0 @@
-const links = []
-links[0] = "https://github.com/rasmus-d";
-links[1] = "https://se.linkedin.com/in/rasmus-danielsson-84607616a?trk=people-guest_people_search-card";
-links[2] = "https://example.org";
-
-
-function newWindow(id) {
-    window.open(links[id], "_blank");
-}
